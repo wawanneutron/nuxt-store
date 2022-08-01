99d0f5c44dad0a878a6b6600dd13d8e14f97dd7d
wawanneutron <wawanneutron1331@gmail.com>
2022-08-02 01:39:21 +0700

    list table data products

---
+++ pages/admin/products/index.vue
@@ -0,0 +1,141 @@
+<template>
+  <main class="c-main">
+    <div class="container-fluid">
+      <div class="fade-in">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card border-0 rounded shadow-sm border-top-orange">
+              <div class="card-header">
+                <span class="font-weight-bold"
+                  ><i class="fa fa-layer-group"></i> PRODUCTS</span
+                >
+              </div>
+              <div class="card-body">
+                <div class="form-group">
+                  <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                      <nuxt-link
+                        :to="{ name: 'admin-products-create' }"
+                        class="btn btn-warning btn-sm"
+                        style="padding-top: 10px"
+                      >
+                        <i class="fa fa-plus-circle"></i> ADD NEW</nuxt-link
+                      >
+                    </div>
+                    <input
+                      v-model="search"
+                      @keypress="searchData"
+                      type="text"
+                      class="form-control"
+                      placeholder="cari berdasarkan nama product"
+                    />
+                    <div class="input-group-append">
+                      <button @click="searchData" class="btn btn-warning">
+                        <i class="fa fa-search"></i>
+                        SEARCH
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <b-table
+                  striped
+                  bordered
+                  hover
+                  :items="products.data"
+                  :fields="fields"
+                  show-empty
+                >
+                </b-table>
+
+                <!-- pagination -->
+                <b-pagination
+                  align="right"
+                  :value="products.current_page"
+                  :total-rows="products.total"
+                  :per-page="products.per_page"
+                  @change="chengePage"
+                  aria-controls="my-table"
+                ></b-pagination>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  // layout
+  layout: "admin",
+
+  // meta
+  head() {
+    return {
+      title: "Products - Administrator",
+    };
+  },
+
+  // data function
+  data() {
+    return {
+      //table header
+      fields: [
+        {
+          label: "Product Name",
+          key: "title",
+        },
+        {
+          label: "Category Name",
+          key: "category.name",
+        },
+        {
+          label: "Stock",
+          key: "stock",
+          tdClass: "text-center",
+        },
+        {
+          label: "Actions",
+          key: "actions",
+          tdClass: "text-center",
+        },
+      ],
+
+      search: "",
+    };
+  },
+
+  // hook "asyncData"
+  async asyncData({ store }) {
+    await store.dispatch("admin/product/getProductsData");
+  },
+
+  // computed
+  computed: {
+    // get data products
+    products() {
+      return this.$store.state.admin.product.products;
+    },
+  },
+
+  // methods
+  methods: {
+    searchData() {
+      // commit to mutation "SET_PAGE"
+      this.$store.commit("admin/product/SET_PAGE", 1);
+
+      // dispatch on actions "getProductsData"
+      this.$store.dispatch("admin/product/getProductsData", this.search);
+    },
+
+    chengePage(page) {
+      // commot to mutation "SET_PAGE"
+      this.$store.commit("admin/product/SET_PAGE", page);
+
+      // dispatch on actions "getProductsData"
+      this.$store.dispatch("admin/product/getProductsData", this.search);
+    },
+  },
+};
+</script>
