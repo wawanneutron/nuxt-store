a722f62e4ac058a21030632ccf2f092f00868aa9
wawanneutron <wawanneutron1331@gmail.com>
2022-08-02 01:38:00 +0700

    nux link in sidebar

--- components/admin/sidebar.vue
+++ components/admin/sidebar.vue
@@ -14,24 +14,24 @@
     <li class="c-sidebar-nav-title">MASTER DATA</li>
 
     <li class="c-sidebar-nav-item">
-      <a href="#" class="c-sidebar-nav-link">
+      <nuxt-link :to="{ name: 'admin-categories' }" class="c-sidebar-nav-link">
         <svg class="c-sidebar-nav-icon">
           <use
             xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-folder"
           ></use>
         </svg>
-        Categories</a
+        Categories</nuxt-link
       >
     </li>
 
     <li class="c-sidebar-nav-item">
-      <a href="#" class="c-sidebar-nav-link">
+      <nuxt-link :to="{ name: 'admin-products' }" class="c-sidebar-nav-link">
         <svg class="c-sidebar-nav-icon">
           <use
             xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-layers"
           ></use>
         </svg>
-        Products</a
+        Products</nuxt-link
       >
     </li>
 
